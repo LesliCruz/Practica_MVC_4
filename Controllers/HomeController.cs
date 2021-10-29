4de814bf51cba7f546104e4dd3ba15456729d073
Lesli Cruz <Leslicruz_23@hotmail.com>
2021-10-28 19:00:30 -0700

    Segunda parte de practica 4 MVC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace Practica_MVC_4.Controllers
             return View();
 
         }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+            return View();
+        }
     }
 }
--- Models/LibroVisitas.cs
+++ Models/LibroVisitas.cs
@@ -16,5 +16,13 @@ namespace Practica_MVC_4.Models
             archivo.WriteLine("Nombre:" + nombre + "<br>Comentarios:" + comentarios + "<hr>");
             archivo.Close();
         }
+
+        public string Leer()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            string todo = archivo.ReadToEnd();
+            archivo.Close();
+            return todo;
+        }
     }
 }
